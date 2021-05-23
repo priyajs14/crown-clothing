b92bd827c3ede93654bd391268ad12cd6e1352df
priyajsingh <priyajsingh@deloitte.com>
2021-05-23 15:36:01 +0530

    initial commit

---
+++ src/Homepage/Homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Homepage.styles.scss';
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item ">
+          <div className="content">
+            <h1 className="title">HAT</h1>
+            <span className="subtitle">shp now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
