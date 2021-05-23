b18c36c543906f14c879d9356dd18231c85a262e
priyajs14 <priyajs14@gmail.com>
2021-05-23 18:20:07 +0530

    header and shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Homepage from './page/homepage/homepage.component';
+import HomePage from './page/homepage/homepage.component';
 import NotFound from './page/not-found/not-found.component';
+import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="app-container">
+        <Header />
         <Switch>
-          <Route exact path="/" component={Homepage} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
           <Route path="/404" component={NotFound} />
           <Redirect to="/404" />
         </Switch>
--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-export default function Homepage() {
+export default function HomePage() {
   return (
-    <div className="homepage">
+    <div className="home-page">
       <Directory />
     </div>
   );
